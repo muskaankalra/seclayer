a01094b4863e0e30acd1627026d2b287d2609ea7
Muskaan Kalra <42842007+muskaankalra@users.noreply.github.com>
2019-05-01 23:36:01 -0400

    New __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,18 @@
 import playground
-from .pls import PIMPServerFactory, PIMPClientFactory
+from .pimp import PIMPServerFactory, PIMPClientFactory
+from .protocol import PLSRoastClient, PLSRoastServer
+from playground.network.common import StackingProtocolFactory
 
-pimpConnector = playground.Connector(protocolStack=(PLSClientFactory(), PLSServerFactory()))
-playground.setConnector("pls", pimpConnector)
-#playground.setConnector("lab1_GoldenNuggetNetSec2019", pimpConnector)
+pimpConnector = playground.Connector(protocolStack=(PIMPClientFactory(), PIMPServerFactory()))
+playground.setConnector("pimp", pimpConnector)
+playground.setConnector("lab1_GoldenNuggetNetSec2019", pimpConnector)
+
+pimpConnector = playground.getConnector("pimp")
+pimpClientType = pimpConnector.getClientStackFactory()
+pimpServerType = pimpConnector.getServerStackFactory()
+
+PlsClientFactory = StackingProtocolFactory.CreateFactoryType(pimpClientType, PLSClientProtocol)
+PlsServerFactory = StackingProtocolFactory.CreateFactoryType(pimpServerType, PLSServerProtocol)
+
+plsConnector = playground.Connector(protocolStack=(PlsClientFactory(),PlsServerFactory()))
+playground.setConnector("pls_roast", plsConnector)
